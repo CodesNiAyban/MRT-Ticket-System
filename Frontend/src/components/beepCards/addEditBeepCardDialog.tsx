2d5034a971d106665cae59b4688c8038e4b6949f
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-01-21 02:58:24 +0800

    [NEW] Generate Beep Card number on creation
    - Creates new beep card with UUIC conditions (starting with "123456" and random 9 digits next)

--- Frontend/src/components/beepCards/addEditBeepCardDialog.tsx
+++ Frontend/src/components/beepCards/addEditBeepCardDialog.tsx
@@ -1,3 +1,4 @@
+// addEditBeepCardDialog.tsx
 import { Button, Form, Modal, Alert } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { BeepCard } from "../../model/beepCardModel";
@@ -7,130 +8,171 @@ import TextInputField from "../form/textInputFields";
 import { useState, useEffect } from "react";
 
 interface AddEditBeepCardDialogProps {
-    beepCardToEdit?: BeepCard,
-    onDismiss: () => void,
-    onBeepCardSaved: (beepCard: BeepCard) => void,
+  beepCardToEdit?: BeepCard;
+  onDismiss: () => void;
+  onBeepCardSaved: (beepCard: BeepCard) => void;
 }
 
-const AddEditBeepCardDialog = ({ beepCardToEdit, onDismiss, onBeepCardSaved }: AddEditBeepCardDialogProps) => {
+const AddEditBeepCardDialog = ({
+  beepCardToEdit,
+  onDismiss,
+  onBeepCardSaved,
+}: AddEditBeepCardDialogProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setValue,
+  } = useForm<BeepCardInput>({
+    defaultValues: {
+      UUIC: beepCardToEdit?.UUIC,
+      balance: beepCardToEdit?.balance,
+    },
+  });
 
-    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<BeepCardInput>({
-        defaultValues: {
-            UUIC: beepCardToEdit?.UUIC,
-            balance: beepCardToEdit?.balance
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+  const [alertVariant, setAlertVariant] = useState<"success" | "danger">(
+    "success"
+  );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowAlert(false);
+      setAlertMessage(null);
+    }, 1300);
+
+    return () => clearTimeout(timeoutId);
+  }, [showAlert]);
+
+  async function onSubmit(input: BeepCardInput) {
+    try {
+      if (!beepCardToEdit) {
+        // Check if UUIC is not generated
+        if (!input.UUIC) {
+          setAlertVariant("danger");
+          setAlertMessage("Please generate UUIC before saving.");
+          setShowAlert(true);
+          return;
         }
-    });
 
-    const [showAlert, setShowAlert] = useState(false);
-    const [alertMessage, setAlertMessage] = useState<string | null>(null);
-    const [alertVariant, setAlertVariant] = useState<'success' | 'danger'>('success');
-
-    useEffect(() => {
-        // Automatically hide the alert after 3 seconds
-        const timeoutId = setTimeout(() => {
-            setShowAlert(false);
-            setAlertMessage(null);
-        }, 1300);
-
-        // Clear the timeout on unmount or when the modal is dismissed
-        return () => clearTimeout(timeoutId);
-    }, [showAlert]);
-
-    async function onSubmit(input: BeepCardInput) {
-        try {
-            let beepCardResponse: BeepCard;
-            if (beepCardToEdit) {
-                // Check if the update remains unchanged
-                if (
-                    input.UUIC === beepCardToEdit.UUIC &&
-                    input.balance === beepCardToEdit.balance
-                ) {
-                    setAlertVariant('danger');
-                    setAlertMessage('Update is unchanged.');
-                    setShowAlert(true);
-                    return;
-                }
-
-                beepCardResponse = await BeepCardsApi.updateBeepCard(beepCardToEdit._id, input);
-            } else {
-                beepCardResponse = await BeepCardsApi.createBeepCard(input);
-            }
-
-            onBeepCardSaved(beepCardResponse);
-        } catch (error: any) {
-            console.error(error);
-
-            if (error.message === "Network Error") {
-                // Handle no connection error
-                setAlertVariant('danger');
-                setAlertMessage('No connection. Please check your internet connection.');
-            } else if (error.response && error.response.status === 409) {
-                // Handle duplicate UUIC error
-                setAlertVariant('danger');
-                setAlertMessage('Error: Duplicate UUIC. Please use a different UUIC.');
-            } else {
-                // Handle other errors
-                setAlertVariant('danger');
-                setAlertMessage('Error saving Beep Card. Please try again.');
-            }
-
-            setShowAlert(true);
+        // Validate UUIC format
+        if (!/^\d{15}$/.test(input.UUIC)) {
+          setAlertVariant("danger");
+          setAlertMessage("Invalid UUIC format. Please enter 15 digits.");
+          setShowAlert(true);
+          return;
         }
+
+        input.balance = 10;
+      }
+
+      let beepCardResponse: BeepCard;
+      if (beepCardToEdit) {
+        if (
+          input.UUIC === beepCardToEdit.UUIC &&
+          input.balance === beepCardToEdit.balance
+        ) {
+          setAlertVariant("danger");
+          setAlertMessage("Update is unchanged.");
+          setShowAlert(true);
+          return;
+        }
+
+        beepCardResponse = await BeepCardsApi.updateBeepCard(
+          beepCardToEdit._id,
+          input
+        );
+      } else {
+        beepCardResponse = await BeepCardsApi.createBeepCard(input);
+      }
+
+      onBeepCardSaved(beepCardResponse);
+    } catch (error: any) {
+      console.error(error);
+
+      if (error.message === "Network Error") {
+        setAlertVariant("danger");
+        setAlertMessage("No connection. Please check your internet connection.");
+      } else if (error.response && error.response.status === 409) {
+        setAlertVariant("danger");
+        setAlertMessage("Error: Duplicate UUIC. Please use a different UUIC.");
+      } else {
+        setAlertVariant("danger");
+        setAlertMessage("Error saving Beep Card. Please try again.");
+      }
+
+      setShowAlert(true);
     }
+  }
 
-    return (
-        <Modal show onHide={onDismiss} centered>
-            <Modal.Header closeButton>
-                <Modal.Title>
-                    {beepCardToEdit ? "Edit beepCard" : "Add beepCard"}
-                </Modal.Title>
-            </Modal.Header>
+  // Function to generate a random 15-digit number with "123456" as the prefix
+  const generateNumber = () => {
+    const generatedUUIC =
+      "123456" +
+      Math.floor(Math.random() * Math.pow(10, 9)).toString().padStart(9, "0");
+    setValue("UUIC", generatedUUIC);
+  };
 
-            <Modal.Body>
-                <Form id="addEditBeepCardForm" onSubmit={handleSubmit(onSubmit)}>
+  return (
+    <Modal show onHide={onDismiss} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>{beepCardToEdit ? "Edit beepCard" : "Add beepCard"}</Modal.Title>
+      </Modal.Header>
 
-                    <TextInputField
-                        name="UUIC"
-                        label="UUIC"
-                        type="text"
-                        placeholder="UUIC"
-                        register={register}
-                        registerOptions={{ required: "Required " }}
-                        errors={errors.UUIC}
-                    />
+      <Modal.Body>
+        <Form id="addEditBeepCardForm" onSubmit={handleSubmit(onSubmit)}>
+          <TextInputField
+            name="UUIC"
+            label="UUIC"
+            type="text"
+            placeholder="UUIC"
+            register={register}
+            registerOptions={{ required: "Required " }}
+            errors={errors.UUIC}
+            readOnly={beepCardToEdit ? false : true}
+          />
 
-                    <TextInputField
-                        name="balance"
-                        label="Balance"
-                        type="number"
-                        placeholder="balance"
-                        register={register}
-                        registerOptions={{ required: "Required " }}
-                        errors={errors.balance}
-                    />
+          {!beepCardToEdit && (
+            <div className="mb-3">
+              <Button variant="secondary" onClick={generateNumber}>
+                Generate Number
+              </Button>
+            </div>
+          )}
 
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button
-                    type="submit"
-                    form="addEditBeepCardForm"
-                    disabled={isSubmitting}
-                >
-                    Save
-                </Button>
-            </Modal.Footer>
+          {beepCardToEdit && (
+            <TextInputField
+              name="balance"
+              label="Balance"
+              type="number"
+              placeholder="balance"
+              register={register}
+              registerOptions={{ required: "Required " }}
+              errors={errors.balance}
+            />
+          )}
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          type="submit"
+          form="addEditBeepCardForm"
+          disabled={isSubmitting}
+        >
+          Save
+        </Button>
+      </Modal.Footer>
 
-            {/* Bootstrap Alert for Success/Error Messages */}
-            {showAlert && (
-                <div className="position-fixed top-50 start-50 translate-middle">
-                    <Alert variant={alertVariant} onClose={() => setShowAlert(false)}>
-                        {alertMessage}
-                    </Alert>
-                </div>
-            )}
-        </Modal>
-    );
-}
+      {showAlert && (
+        <div className="position-fixed top-50 start-50 translate-middle">
+          <Alert variant={alertVariant} onClose={() => setShowAlert(false)}>
+            {alertMessage}
+          </Alert>
+        </div>
+      )}
+    </Modal>
+  );
+};
 
 export default AddEditBeepCardDialog;
--- Frontend/src/model/beepCardModel.ts
+++ Frontend/src/model/beepCardModel.ts
@@ -1,7 +1,7 @@
 export interface BeepCard {
     _id: string,
     UUIC: string,
-    balance: number,
+    balance?: number,
     createdAt: string,
     updatedAt: string,
 }
